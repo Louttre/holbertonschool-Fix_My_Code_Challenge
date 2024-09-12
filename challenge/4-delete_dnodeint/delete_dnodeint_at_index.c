2fc1c2adaa3f24b844265fc80ea2f290f8784efe
root <seth.rueda@gmail.com>
2024-09-12 10:08:59 +0000

    fix all the codes

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -4,50 +4,54 @@
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
- * @head: A pointer to the first element of a list
- * @index: The index of the node to delete
+ * @head: A pointer to the first element of a doubly linked list
+ * @index: The index of the node to delete (starting from 0)
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+    dlistint_t *current;
+    unsigned int p;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
-	return (1);
+    /* If the list is empty, return failure */
+    if (*head == NULL)
+        return (-1);
+
+    current = *head;
+    p = 0;
+
+    /* Traverse to the node at the specified index */
+    while (p < index && current != NULL)
+    {
+        current = current->next;
+        p++;
+    }
+
+    /* If the index is out of bounds, return failure */
+    if (current == NULL)
+        return (-1);
+
+    /* Deleting the head (first node) */
+    if (index == 0)
+    {
+        *head = current->next;
+        if (*head != NULL)  /* If there is a next node, update its prev pointer */
+            (*head)->prev = NULL;
+    }
+    else
+    {
+        /* Update the previous node's next pointer */
+        if (current->prev != NULL)
+            current->prev->next = current->next;
+
+        /* Update the next node's prev pointer if it's not the last node */
+        if (current->next != NULL)
+            current->next->prev = current->prev;
+    }
+
+    /* Free the memory of the node to be deleted */
+    free(current);
+    return (1);
 }
+
